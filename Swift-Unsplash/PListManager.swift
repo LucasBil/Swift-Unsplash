ce69be93cc32f8743d07704889128486af70b3f8
LucasBil <LucasBilly@etu.univ-savoie.fr>
2024-01-23 16:14:11 +0100

    gitignore

---
+++ Swift-Unsplash/PListManager.swift
@@ -0,0 +1,8 @@
+//
+//  PListManager.swift
+//  Swift-Unsplash
+//
+//  Created by Lucas BILLY on 1/23/24.
+//
+
+import Foundation
---
+++ Swift-Unsplash/UnsplashImage.swift
@@ -0,0 +1,8 @@
+//
+//  UnsplashImage.swift
+//  Swift-Unsplash
+//
+//  Created by Lucas BILLY on 1/23/24.
+//
+
+import Foundation
