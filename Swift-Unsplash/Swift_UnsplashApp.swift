f3e1f19a40cf8cea2a32b73e51c4ddf456022371
LucasBil <LucasBilly@etu.univ-savoie.fr>
2024-01-23 13:39:17 +0100

    Initial Commit

---
+++ Swift-Unsplash/Swift_UnsplashApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_UnsplashApp.swift
+//  Swift-Unsplash
+//
+//  Created by Lucas BILLY on 1/23/24.
+//
+
+import SwiftUI
+
+@main
+struct Swift_UnsplashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
