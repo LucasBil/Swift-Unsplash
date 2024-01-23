1eb6bae1fa8d94b6d86a7cdf48a8d701289c65bb
LucasBil <LucasBilly@etu.univ-savoie.fr>
2024-01-23 17:26:48 +0100

    advanced

---
+++ Swift-Unsplash/FeedState.swift
@@ -0,0 +1,8 @@
+//
+//  FeedState.swift
+//  Swift-Unsplash
+//
+//  Created by Lucas BILLY on 1/23/24.
+//
+
+import Foundation
---
+++ Swift-Unsplash/UnsplashAPI.swift
@@ -0,0 +1,8 @@
+//
+//  UnsplashAPI.swift
+//  Swift-Unsplash
+//
+//  Created by Lucas BILLY on 1/23/24.
+//
+
+import Foundation
